f8d84b7c733a77c94ff081d77568b239fffc9632
Jules AFRESNE <jules.afresne@groupama.com>
2026-06-10 11:14:14 +0200

    feat(no-ticket): add SpeedInsights component and update dependencies

--- main.tsx
+++ main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import App from "./fin";
 
 import "./style.css";
@@ -19,5 +20,6 @@ createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <App />
         <Analytics />
+        <SpeedInsights />
     </React.StrictMode>,
 );
